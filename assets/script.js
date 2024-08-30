60460ebfa1683bffe4376c2027e427ab3c875d3f
Guillermo Croppi <guillermocroppi@gmail.com>
2024-08-30 20:43:38 -0300

    chore: Update font sizes for different screen sizes

--- assets/script.js
+++ assets/script.js
@@ -7,4 +7,21 @@ $(document).ready(function() {
       $(this).removeClass('is-active');
     }
   );
+
+  $('.navbar-burger').each(function() {
+    var $this = $(this);
+
+    // Add a click event on each of them
+    $this.click(function() {
+
+      // Get the target from the "data-target" attribute
+      var target = $this.data('target');
+      var $target = $('#' + target);
+
+      // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+      $this.toggleClass('is-active');
+      $target.toggleClass('is-active');
+
+    });
+  });
 });
